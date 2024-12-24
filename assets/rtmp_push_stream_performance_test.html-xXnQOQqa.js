d08cb4f787b89989a00b69cac3c1b35f70646c08
wasphin <wasphin@gmail.com>
2024-12-24 13:35:25 +0000

    Deploying to gh-pages from @ ZLMediaKit/docs@1d2e591c85a22c64852a4dfaec8fe20af416b081 🚀

---
+++ assets/rtmp_push_stream_performance_test.html-xXnQOQqa.js
@@ -0,0 +1,43 @@
+import{_ as l}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as a,o as r,c as d,a as e,b as s,d as t,e as m}from"./app-CRnFokh3.js";const p={},u={href:"https://raw.githubusercontent.com/ossrs/srs/develop/trunk/doc/source.200kbps.768x320.flv",target:"_blank",rel:"noopener noreferrer"};function o(v,n){const i=a("ExternalLinkIcon");return r(),d("div",null,[n[12]||(n[12]=e("h2",{id:"一、测试环境",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#一、测试环境","aria-hidden":"true"},"#"),s(" 一、测试环境")],-1)),e("ul",null,[n[2]||(n[2]=e("li",null,"测试日期：2022/5/18",-1)),n[3]||(n[3]=e("li",null,"代码版本：git hash: c7d7999f",-1)),n[4]||(n[4]=e("li",null,"cpu： Intel(R) Xeon(R) Gold 6148 CPU @ 2.40GHz",-1)),n[5]||(n[5]=e("li",null,"操作系统：CentOS release 6.3 (Final)",-1)),n[6]||(n[6]=e("li",null,"内存：16GB",-1)),n[7]||(n[7]=e("li",null,"网卡：127.0.0.1",-1)),e("li",null,[n[1]||(n[1]=s("测试码流: ")),e("a",u,[n[0]||(n[0]=s("200kbps.768x320.flv")),t(i)])]),n[8]||(n[8]=e("li",null,"编译器：gcc (GCC) 8.2.0",-1)),n[9]||(n[9]=e("li",null,"zlmediakit 编译类型：Release",-1)),n[10]||(n[10]=e("li",null,"malloc 库：ptmalloc(未开启 jemalloc)",-1)),n[11]||(n[11]=e("li",null,"config.ini 配置文件修改(主要开启合并写、按需转协议)：",-1))]),n[13]||(n[13]=m(`<div class="language-patch line-numbers-mode" data-ext="patch"><pre class="language-patch"><code>diff --git a/conf/config.ini b/conf/config.ini
+index c2d4613f..99ce5c84 100644
+--- a/conf/config.ini
++++ b/conf/config.ini
+@@ -51,12 +51,12 @@ addMuteAudio=1
+ #如果不删除将会接着上一次的数据继续写(录制hls/mp4时会继续在前一个文件后面写)
+ resetWhenRePlay=1
+ #是否默认推流时转换成hls，hook接口(on_publish)中可以覆盖该设置
+-publishToHls=1
++publishToHls=0
+ #是否默认推流时mp4录像，hook接口(on_publish)中可以覆盖该设置
+ publishToMP4=0
+ #合并写缓存大小(单位毫秒)，合并写指服务器缓存一定的数据后才会一次性写入socket，这样能提高性能，但是会提高延时
+ #开启后会同时关闭TCP_NODELAY并开启MSG_MORE
+-mergeWriteMS=0
++mergeWriteMS=300
+ #全局的时间戳覆盖开关，在转协议时，对frame进行时间戳覆盖
+ #该开关对rtsp/rtmp/rtp推流、rtsp/rtmp/hls拉流代理转协议时生效
+ #会直接影响rtsp/rtmp/hls/mp4/flv等协议的时间戳
+@@ -72,15 +72,15 @@ enable_audio=1
+ ####### 如果某种协议你想获取最好的用户体验，请置0(第一个播放者可以秒开，且不花屏)
+
+ #hls协议是否按需生成，如果hls.segNum配置为0(意味着hls录制)，那么hls将一直生成(不管此开关)
+-hls_demand=0
++hls_demand=1
+ #rtsp[s]协议是否按需生成
+-rtsp_demand=0
++rtsp_demand=1
+ #rtmp[s]、http[s]-flv、ws[s]-flv协议是否按需生成
+-rtmp_demand=0
++rtmp_demand=1
+ #http[s]-ts协议是否按需生成
+-ts_demand=0
++ts_demand=1
+ #http[s]-fmp4、ws[s]-fmp4协议是否按需生成
+-fmp4_demand=0
++fmp4_demand=1
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>推流命令:</li></ul><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>ffmpeg <span class="token parameter variable">-stream_loop</span> <span class="token parameter variable">-1</span> <span class="token parameter variable">-re</span> <span class="token parameter variable">-i</span> ~/Downloads/source.200kbps.768x320.flv <span class="token parameter variable">-acodec</span> copy <span class="token parameter variable">-vcodec</span> copy <span class="token parameter variable">-f</span> flv  rtmp://ip:port/live/test
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="二、rtmp-推流性能测试-1-万路" tabindex="-1"><a class="header-anchor" href="#二、rtmp-推流性能测试-1-万路" aria-hidden="true">#</a> 二、rtmp 推流性能测试(1 万路)</h2><ul><li>推流命令：</li></ul><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment">#加大文件描述符个数</span>
+<span class="token builtin class-name">ulimit</span> <span class="token parameter variable">-n</span> <span class="token number">102400</span>
+<span class="token comment">#启动1万个rtmp推流</span>
+./test_bench_push <span class="token parameter variable">-i</span> rtmp://127.0.0.1/live/test <span class="token parameter variable">-c</span> <span class="token number">10000</span> <span class="token parameter variable">-o</span> rtmp://127.0.0.1/live/push
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>top 信息：</li></ul><figure><img src="https://user-images.githubusercontent.com/11495632/169197952-9a042459-a3fe-4b48-903b-4aa0095dfe77.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><ul><li>perf top 信息：</li></ul><figure><img src="https://user-images.githubusercontent.com/11495632/169198018-1a823b57-4e9f-4331-950d-329178c40de8.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure><ul><li>nload 信息(平均 2.16Gb/s)：</li></ul><figure><img src="https://user-images.githubusercontent.com/11495632/169198085-5c4278f1-c4dc-421a-992c-aac94d090479.png" alt="图片" tabindex="0" loading="lazy"><figcaption>图片</figcaption></figure>`,12))])}const f=l(p,[["render",o],["__file","rtmp_push_stream_performance_test.html.vue"]]);export{f as default};
